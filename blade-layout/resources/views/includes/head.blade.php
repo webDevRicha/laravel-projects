f34f5bd306fe70050a863accadb4260d91a379fa
Richa <richa.parekh@pavansgroup.in>
2024-05-05 22:33:59 +0530

    Create basic blade layout

---
+++ blade-layout/resources/views/includes/head.blade.php
@@ -0,0 +1,6 @@
+<meta charset="utf-8">
+<meta name="description" content="Basic blade layout">
+<meta name="Richa" content="Blade Layout">
+<title>Basic blade layout</title>
+<!-- load bootstrap from a cdn -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/3.0.3/css/bootstrap-combined.min.css">
--- blade-layout/routes/web.php
+++ blade-layout/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
+});
+Route::get('/contact', function()
+{
+   return view('pages.contact');
 });
